fba389c624d68783ec083a8fdaa5d783c65dee80
Syed Muhammad Ali Rizvi <63605517+Ali-Rizvi-1@users.noreply.github.com>
2021-12-12 18:16:24 +0500

    Final Update

--- script.m
+++ script.m
@@ -1,5 +1,25 @@
+clc; clear;
+
 m = 0.5; %Kg
 l = 1; %meter
 g = 9.8; %m/s^2
 
 sim('FeedbackLinearizationModel')
+
+%plot(time, PlantOutput)
+time = ans.time;
+plant_output = ans.PlantOutput;
+desired_output = ans.DesiredOutput;
+
+plot(time,plant_output,'linewidth',2); hold on
+plot(time,desired_output,'linewidth',2);
+legend('Plant Output','Desired Output')
+title('Controller based on Feedback Linearization')
+
+print -dpng comparison 
+
+% Error plotting
+error = desired_output-plant_output;
+figure;
+plot(time,error,'linewidth',1);
+title('Error between desired and current output')
